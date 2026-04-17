fed6ba6ab0005992e0b816ba3dc144ef43d2fcb4
Konststep <Rain1-1@mail.ru>
2026-04-17 22:21:55 +0300

    Задание 1

---
+++ zadanie.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	
+	std::cout << "Vvedi chislo " << std::endl;
+	std::cin >> a;
+	std::cout << "Vvedennoe chislo: " << std::endl;
+	std::cout << a << std::endl;
+
+	return EXIT_SUCCESS;
+}
